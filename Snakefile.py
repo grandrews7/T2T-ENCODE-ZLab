ea8cf66c358cfc240fbc8f763be2d3c3830483d3
Christian Ramirez <ramirezc@z014.internal.wenglab.org>
2024-07-18 13:45:58 -0400

    drafted trimmomatic snakemake rule

--- Snakefile.py
+++ Snakefile.py
@@ -23,8 +23,8 @@ rule all:
     input: 
         expand(workDir + "/kmer/{genome}-k{kmer}.bigWig", genome=genomes, kmer=kmers),
         expand(workDir + "/genome/{genome}.fa.1.bt2", genome=genomes),
-        expand(workDir + "/fastq1/{acc}.fastq.gz", acc=acc_list),
-        expand(workDir + "/fastq2/{exp}_{read}.fastq.gz", exp=experiments, read=[str(i) for i in range(1,3)])
+        expand(workDir + "/fastq-raw/{acc}.fastq.gz", acc=acc_list),
+        expand(workDir + "/fastq-merged/{exp}_{read}.fastq.gz", exp=experiments, read=[str(i) for i in range(1,3)])
 
 # rule all:
 #     input:
@@ -34,7 +34,7 @@ rule all:
 ruleorder: get_fastq > merge_fastq
 
 rule get_fastq:
-    output: temp(workDir + "/fastq1/{acc}.fastq.gz")
+    output: temp(workDir + "/fastq-raw/{acc}.fastq.gz")
     threads: 1
     params: url = lambda w: URLs[w.acc]
     shell:
@@ -43,7 +43,7 @@ rule get_fastq:
         """
 
 def get_exp_fastq_list(wildcards):
-    return([workDir + "/fastq1/" + acc + ".fastq.gz" for acc in metadata[metadata["exp"] == wildcards.exp].acc.tolist()])
+    return([workDir + "/fastq-raw/" + acc + ".fastq.gz" for acc in metadata[metadata["exp"] == wildcards.exp].acc.tolist()])
 
 def get_R1(exp):
     return(metadata[(metadata["exp"] == exp) & (metadata["read"] == 1)].acc.tolist())
@@ -54,29 +54,46 @@ def get_R2(exp):
 rule merge_fastq:
     input: get_exp_fastq_list
     output: 
-        fq1 = workDir + "/fastq2/{exp}_1.fastq.gz",
-        fq2 = workDir + "/fastq2/{exp}_2.fastq.gz"
+        fq1 = workDir + "/fastq-merged/{exp}_1.fastq.gz",
+        fq2 = workDir + "/fastq-merged/{exp}_2.fastq.gz"
     threads: 1
     params:
-        fastq1Dir = workDir + "/fastq1",
-        fastq2Dir = workDir + "/fastq2",
+        fastq-rawDir = workDir + "/fastq-raw",
+        fastq-mergedDir = workDir + "/fastq-merged",
         R1 = lambda w: get_R1(w.exp),
         R2 = lambda w: get_R2(w.exp)
     shell:
         """
         for acc in {params.R1}; do
-            zcat {params.fastq1Dir}/$acc.fastq.gz >> {params.fastq2Dir}/{wildcards.exp}_1.fastq
+            zcat {params.fastq-rawDir}/$acc.fastq.gz >> {params.fastq-mergedDir}/{wildcards.exp}_1.fastq
         done
 
         for acc in {params.R2}; do
-            zcat {params.fastq1Dir}/$acc.fastq.gz >> {params.fastq2Dir}/{wildcards.exp}_2.fastq
+            zcat {params.fastq-rawDir}/$acc.fastq.gz >> {params.fastq-mergedDir}/{wildcards.exp}_2.fastq
         done
-        gzip {params.fastq2Dir}/{wildcards.exp}_1.fastq
-        gzip {params.fastq2Dir}/{wildcards.exp}_2.fastq
+        gzip {params.fastq-mergedDir}/{wildcards.exp}_1.fastq
+        gzip {params.fastq-mergedDir}/{wildcards.exp}_2.fastq
         #touch {output.fq1}
         #touch {output.fq2}
         """
-        
+
+rule trimmomatic:
+    input: 
+        fq1 = workDir + "/fastq-merged/{exp}_1.fastq",
+        fq2 = workDir + "/fastq-merged/{exp}_2.fastq"
+    output: 
+        fq1Paired = temp(workDir + "/fastq-trimmed/{exp}_1.paired.fastq")
+        fq1Unpaired = temp(workDir + "/fastq-trimmed/{exp}_1.unpaired.fastq")
+        fq1Paired = temp(workDir + "/fastq-trimmed/{exp}_2.paired.fastq")
+        fq2Unpaired = temp(workDir + "/fastq-trimmed/{exp}_1.unpaired.fastq")
+    threads: 24
+    params:
+        step = "string"
+    shell:
+        """
+        java -jar trimmomatic-0.39 PE -phred64 -threads {threads} {input.fq1} {input.fq2} {output.fq1Paired} {output.fq1Unpaired} {output.fq2Paired} {output.fq2Unpaired} {params.step}
+        """
+
 rule get_genome:
     output:
         fa = workDir + "/genome/{genome}.fa",
@@ -129,8 +146,6 @@ rule bowtie2_build:
         bowtie2-build --threads {threads} {wildcards.genome}.fa {wildcards.genome}.fa
         """
     
-
-
 rule kmc:
     input:
         fa = workDir + "/genome/{genome}.fa",
