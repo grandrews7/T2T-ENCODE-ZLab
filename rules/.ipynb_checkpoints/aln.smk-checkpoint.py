825528f22008e036bd8ae915312e4b815fa3e067
Christian Ramirez <ramirezc@z014.internal.wenglab.org>
2024-08-07 16:37:24 -0400

    restaging
    changing tracked files\nnext line test here

---
+++ rules/.ipynb_checkpoints/aln.smk-checkpoint.py
@@ -0,0 +1,175 @@
+rule bowtie2_align_se:
+    input:
+        workDir + "/fastq-trimmed-se/{exp}-{biorep}.fastq.gz",
+    
+    output:
+        workDir + "/aln-raw-se/{genome}-{exp}-{biorep}.bam",
+
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=720,
+        nodes=1,
+        slurm_partition="12hours"
+    log:
+        workDir + "/logs/bowtie2_align_se/{genome}-{exp}-{biorep}.log"
+    singularity:
+        "docker://andrewsg/t2t-encode"
+    shell:
+        """
+        (
+        echo "Running bowtie2 align"
+        /opt/bowtie2/bowtie2 --no-discordant --no-mixed --very-sensitive --no-unal --omit-sec-seq --xeq --reorder \
+        --threads {threads} \
+        -x /zata/zippy/andrewsg/scratch/genome/{wildcards.genome}.fa \
+        --rg-id {wildcards.exp}-{wildcards.biorep} \
+        --rg SM:{wildcards.exp}-{wildcards.biorep} \
+        -U {input} | \
+        samtools view -@{threads} \
+        -o {output}
+        ) &> {log}
+        """
+
+    
+rule bowtie2_align_pe:
+    input:
+        workDir + "/fastq-trimmed-pe/{exp}-{biorep}_1.P.fastq.gz", 
+        workDir + "/fastq-trimmed-pe/{exp}-{biorep}_2.P.fastq.gz",
+
+    output:
+        workDir + "/aln-raw-pe/{genome}-{exp}-{biorep}.bam",
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=720,
+        nodes=1,
+        slurm_partition="12hours"   
+    log:
+        workDir + "/logs/bowtie2_align_pe/{genome}-{exp}-{biorep}.log"
+    singularity:
+        "docker://andrewsg/t2t-encode"
+    shell:
+        """
+        (
+        echo "Running bowtie2 align"
+        /opt/bowtie2/bowtie2 --no-discordant --no-mixed --very-sensitive --no-unal --omit-sec-seq --xeq --reorder \
+        --threads {threads} \
+        -x /zata/zippy/andrewsg/scratch/genome/{wildcards.genome}.fa \
+        --rg-id {wildcards.exp}-{wildcards.biorep} \
+        --rg SM:{wildcards.exp}-{wildcards.biorep} \
+        -1 {input[0]} \
+        -2 {input[1]} | \
+        samtools view -@{threads} \
+        -o {output}
+        ) &> {log}
+        """
+
+def get_bams(wildcards):
+    
+    tmp_df = metadata[(metadata["exp"] == wildcards.exp) & (metadata["biorep"] == int(wildcards.biorep))].reset_index(drop=True)
+    read_type = tmp_df.run_type.tolist()[0]
+    if read_type == "paired-ended":
+        dir = workDir + "/aln-raw-pe"
+    else:
+        dir = workDir + "/aln-raw-se"
+
+    bams = []
+    bioreps = tmp_df.biorep.unique().tolist()
+    for b in bioreps:
+        bams.append(dir + "/" + wildcards.genome + "-" + wildcards.exp + "-" + str(b) + ".bam")
+    return(bams)
+
+def get_flag(w):
+    run_type = RUN_TYPES[w.exp]
+    if run_type == "paired-ended":
+        return(["1804", "2"])
+    else:
+        return(["1796", "0"])
+        
+    
+    
+rule filter_bam:
+    input: get_bams
+    output: workDir + "/aln-filtered/{genome}-{exp}-{biorep}.bam"
+    threads: 16
+    resources:
+        mem_mb=60000,
+        c=8,
+        runtime=720,
+        nodes=1,
+        slurm_partition="12hours"  
+    log:
+        workDir + "/logs/filter_bam/{genome}-{exp}-{biorep}.log"
+    singularity:
+        "docker://andrewsg/t2t-encode"
+    params:
+            flag = lambda w: get_flag
+    shell:
+        """
+        (
+        echo "Sorting by coordinate"
+        samtools sort -@ {threads} -o {wildcards.genome}-{wildcards.exp}-{wildcards.biorep}.sort.bam {input}
+        echo "Indexing BAM file"
+        samtools index -@ {threads} {wildcards.genome}-{wildcards.exp}-{wildcards.biorep}.sort.bam
+        
+
+        java -jar /opt/picard/build/libs/picard.jar MarkDuplicates \
+            INPUT={wildcards.genome}-{wildcards.exp}.sort.bam \
+            OUTPUT={wildcards.genome}-{wildcards.exp}.markdup.bam \
+            METRICS_FILE={wildcards.genome}-{wildcards.exp}.markdup.txt \
+            ASSUME_SORT_ORDER=coordinate
+
+        echo "Filtering BAM file (-f {params.flag[0]}; -F {params.flag[1]})"
+        samtools view -b -F {params.flag[0]} -f {params.flag[1]} -q 2 \
+        -@{threads} \
+        -o  {output} \
+        {wildcards.genome}-{wildcards.exp}.markdup.bam
+        rm {wildcards.genome}-{wildcards.exp}.merge.bam
+        rm {wildcards.genome}-{wildcards.exp}.sort.bam
+        rm {wildcards.genome}-{wildcards.exp}.markdup.bam
+        ) &> {log}
+        """
+
+
+# Create dictionary of experiments and their read type (single-ended or paired-end)
+
+rule kmer_filter:
+    input: 
+        bam = workDir + "/aln-filtered/{genome}-{exp}-{biorep}.bam",
+        kmer_bigWigs = [workDir + "/kmer/{genome}-k" + _ + ".bw" for _ in kmers]
+    output:
+        workDir + "/aln-final/{genome}-{exp}-{biorep}.bam"
+    threads: 32
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=720,
+        nodes=1,
+        slurm_partition="12hours"   
+    log:
+        workDir + "/logs/kmer_filter/{genome}-{exp}-{biorep}.log"
+    singularity:
+        "docker://andrewsg/t2t-encode"
+    params:
+        run_type = lambda w: RUN_TYPES[w.exp],
+        script = scriptDir + "/filter_by_unique_kmers.py",
+        kmer_bigWigs = lambda w, input: ",".join(input.kmer_bigWigs)
+    shell:
+        """
+        if [ {params.run_type} == "paired-ended" ]; then
+            echo "Sorting paired-ended BAM by name"
+            samtools sort -n -@ {threads} -o {wildcards.genome}-{wildcards.exp}-{wildcards.biorep}.namesort.bam {input.bam}
+            python3 {params.script} -b {wildcards.genome}-{wildcards.exp}-{wildcards.biorep}.namesort.bam \
+            -k {params.kmer_bigWigs} \
+            -r {params.run_type} \
+            -o {output}
+        else
+            echo "single-ended"
+            python3 {params.script} -b {input.bam} \
+            -k {params.kmer_bigWigs} \
+            -r {params.run_type} \
+            -o {output}
+        fi
+        """
---
+++ rules/.ipynb_checkpoints/fastq.smk-checkpoint.py
@@ -0,0 +1,146 @@
+# download individual fastq files
+rule get_fastq:
+    output: workDir + "/fastq-raw/{acc}.fastq.gz",
+    threads: 1
+    log:
+        workDir + "/logs/get_fastq/{acc}.log"
+    resources:
+        mem_mb=4000,
+        c=1,
+        runtime=240,
+        nodes=1,
+        slurm_partition="4hours"
+    singularity:
+        "docker://andrewsg/t2t-encode",
+    params: 
+        url = lambda w: URLs[w.acc],
+        md5sum = lambda w: MD5SUMs[w.acc]
+    shell:
+        """
+        (
+        echo "Downloading fastq files"
+        checksum=""
+        while [ "$checksum" != {params.md5sum} ]
+        do
+            wget -q -O {output} {params.url}
+            checksum=$(md5sum {output} | awk '{{print $1; exit}}')
+        done
+        ) &> {log}
+        """
+
+def get_fastq_list_se(wildcards):
+    return([workDir + "/fastq-raw/" + acc + ".fastq.gz" for acc in metadata[(metadata["exp"] == wildcards.exp) & (metadata["biorep"] == int(wildcards.biorep))].acc.tolist()])
+    
+rule merge_fastq_se:
+    input: get_fastq_list_se
+    output: workDir + "/fastq-merged-se/{exp}-{biorep}.fastq.gz" 
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=240,
+        nodes=1,
+        slurm_partition="4hours"
+    log:
+        workDir + "/logs/merge_fastq_se/{exp}-{biorep}.log"
+    params:
+        inp_len = lambda wildcards, input: len(input)
+    shell:
+        """
+        (
+        echo {input}
+        echo {params.inp_len}
+        if [[ {params.inp_len} -eq 1 ]]; then
+            mv {input} {output}
+        else
+            pigz -cd -p {threads} {input} | pigz -c -p {threads} > {output} 
+        fi
+        ) &> {log}
+        """
+
+def get_fastq_list_pe(wildcards):
+    tmp_df = metadata[(metadata["exp"] == wildcards.exp) & (metadata["biorep"] == int(wildcards.biorep)) & (metadata["read"] == int(wildcards.read))]
+    return([workDir + "/fastq-raw/" + acc + ".fastq.gz" for acc in tmp_df.sort_values("read_count").acc.tolist()])
+    
+rule merge_fastq_pe:
+    input: get_fastq_list_pe
+    output: workDir + "/fastq-merged-pe/{exp}-{biorep}_{read}.fastq.gz"
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=240,
+        nodes=1,
+        slurm_partition="4hours"
+    log:
+        workDir + "/logs/merge_fastq_pe/{exp}-{biorep}_{read}.log"
+    params:
+        inp_len =lambda wildcards, input: len(input)
+    shell:
+        """
+        (
+        echo {input}
+        echo {params.inp_len}
+        if [[ {params.inp_len} -eq 1 ]]; then
+            mv {input} {output}
+        else
+            pigz -cd -p {threads} {input} | pigz -c -p {threads} > {output} 
+        fi
+        ) &> {log}
+        """
+
+rule trim_pe:
+    input: 
+        R1 = workDir + "/fastq-merged-pe/{exp}-{biorep}_1.fastq.gz",
+        R2 = workDir + "/fastq-merged-pe/{exp}-{biorep}_2.fastq.gz"
+    output:
+        fq1_paired = workDir + "/fastq-trimmed-pe/{exp}-{biorep}_1.P.fastq.gz",
+        fq1_unpaired = workDir + "/fastq-trimmed-pe/{exp}-{biorep}_1.U.fastq.gz",
+        fq2_paired = workDir + "/fastq-trimmed-pe/{exp}-{biorep}_2.P.fastq.gz",
+        fq2_unpaired = workDir + "/fastq-trimmed-pe/{exp}-{biorep}_2.U.fastq.gz"
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=8,
+        runtime=240,
+        nodes=1,
+        slurm_partition="4hours"
+    log:
+        workDir + "/logs/trim_pe/{exp}-{biorep}.log",
+    singularity:
+        "docker://andrewsg/t2t-encode",
+    params:
+        trimmomatic = "/opt/Trimmomatic-0.39/trimmomatic-0.39.jar",
+        trim_method = "LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36",
+    shell:
+        """
+        (
+        echo "Trimming fastq files"
+        java -jar {params.trimmomatic} PE -phred33 -threads {threads} {input.R1} {input.R2} {output.fq1_paired} {output.fq1_unpaired} {output.fq2_paired} {output.fq2_unpaired} {params.trim_method}
+        ) &> {log}
+        """
+
+rule trim_se:
+    input: workDir + "/fastq-merged-se/{exp}-{biorep}.fastq.gz",
+    output: workDir + "/fastq-trimmed-se/{exp}-{biorep}.fastq.gz",
+    threads: 8
+    resources:
+        mem_mb=32000,
+        c=24,
+        runtime=240,
+        nodes=1,
+        slurm_partition="4hours" 
+    log:
+        workDir + "/logs/trim_se/{exp}-{biorep}.log",
+    singularity:
+        "docker://andrewsg/t2t-encode",
+    params:
+        trimmomatic = "/opt/Trimmomatic-0.39/trimmomatic-0.39.jar",
+        trim_method = "LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36",
+    shell:
+        """
+        (
+        echo "Trimming fastq files"
+        java -jar {params.trimmomatic} SE -phred33 -threads {threads} {input} {output} {params.trim_method}
+        ) &> {log}
+        """
